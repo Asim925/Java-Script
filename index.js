8910391dadb1a575695b693a0398184ecce5fcbc
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-14 01:29:58 +0500

    new code

---
+++ index.js
@@ -0,0 +1,27 @@
+// First code of JAVA-SCRIPT
+// printing hello world
+console.log("Hello World");
+
+// variable
+let me = "asim";
+console.log(me);
+// constants
+const water = "H2O";
+console.log(water);
+// variable {both properties are been shown here}
+let person = {
+  age: "30",
+  color: "red",
+};
+//pointing a property
+person.color = "green";
+// printing
+console.log(person);
+
+// Array
+let colors = ["red", "blue"];
+// adding another element manually
+colors[2] = "black";
+console.log(colors);
+// printing one of them
+console.log(colors[1]);
