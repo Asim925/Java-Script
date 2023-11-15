f644772011ef1b5a2e87d85103dc46a133c0f7d1
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-16 00:40:46 +0500

    Relational operators

---
+++ Operators/Relational operators/index.js
@@ -0,0 +1,10 @@
+let x = 1;
+console.log(x);
+
+// Relational operators
+console.log(x > 1);
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+console.log(x === 0);
+console.log(x !== 0);
