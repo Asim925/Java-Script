05f133eb89b8e6a09aa7d8c65ea3f76f24ebaf5c
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-16 00:55:16 +0500

    equality types

---
+++ Operators/Relational operators/Equality/index.js
@@ -0,0 +1,10 @@
+// strict Equality  (checks value & type)
+console.log(1 === 1);
+console.log(1 !== 1);
+
+// lose Equality  (checks only value){type khud change kr ke value match kr deta hai}
+// console.log(true == 1) ....EXAMPLE ^
+console.log(1 == 1);
+console.log(1 != 1);
+
+/*..stict wale  */
