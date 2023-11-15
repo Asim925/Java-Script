5e809970382c1e974c23063b1638da61f8ccbd34
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-16 01:08:13 +0500

    ternary operators

---
+++ Operators/ternary operators/index.js
@@ -0,0 +1,6 @@
+// TERNARY OPERATORS: if else wala cheez i think,...
+
+/* check if the customer is ameer ot ghareeb  by using their points*/
+let points = 100;
+let customer = points > 85 ? "ameer customer" : "ghareeb customer";
+console.log(customer);
