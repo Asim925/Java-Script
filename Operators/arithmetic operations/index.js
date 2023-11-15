689dfcb63ebf933771067cc87ae55e611e4ea786
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-16 00:18:52 +0500

    arithmetic operators

---
+++ Operators/arithmetic operations/index.js
@@ -0,0 +1,20 @@
+let x = 5;
+let y = 2;
+
+// assignment operators
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+// x's power y
+console.log(x ** y);
+
+// increment operator
+console.log(++x);
+console.log(y++);
+console.log(y);
+// deccrement operator
+console.log(--x);
+console.log(y--);
+console.log(y);
