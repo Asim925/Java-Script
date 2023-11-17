fb3def60dba3d71a7e944b467d322ea5aaa05970
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-18 00:47:33 +0500

    loops

--- Operators/control flow/index.js
+++ Operators/control flow/index.js
@@ -2,10 +2,58 @@
 /* conditions are 
 if time is between 6 and 12 , then print Good morning! 
 if time is between 12 and 6 , them print Good afternoon!
-else good evening
-*/
-let hour = 3;
+else good evening*/
+
+// =====CODE STARTS====== //
+// if-else
+
+/*let hour = 3;
 
 if (hour >= 6 && hour < 12) console.log("Good Morning");
 else if (hour >= 12 && hour < 6) console.log("Good Afternoon");
-else console.log("Good Evening");
+else console.log("Good Evening"); */
+
+// =====CODE ENDS====== //
+
+// ========================= //
+
+// =====CODE STARTS HERE====== //
+// SWITCH statements
+
+/* let user = "guest";
+
+switch (user) {
+  case "guest":
+    console.log("guest - user");
+    break;
+
+  case "member":
+    console.log("Member");
+    break;
+
+  case "head":
+    console.log("Head");
+    break;
+
+  default:
+    console.log("unknown user");
+} */
+// =====CODE ENDS====== //
+
+// ========================= //
+
+// =====CODE STARTS HERE====== //
+// FOR lOOP
+// .....for(initialize , condition , increment or decrement operator )....
+
+/*let i;
+for (i = 0; i < 5; i++) {
+  console.log("Hello");
+}*/
+
+// for printing odd values by looping
+/* let i;
+for (i = 0; i <= 5; i++) {
+  // agr i ko 2 se divide krne pr remainder na bache to woh odd numbers
+  if (i % 2 !== 0) console.log(i);
+} */
