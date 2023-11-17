df8a15a897eacb669dd8111feb981ec523f3906d
Asim Nazeer <asimnazeer925@gmail.com>
2023-11-17 23:30:18 +0500

    if-else

---
+++ Operators/control flow/index.js
@@ -0,0 +1,11 @@
+// if-else statements
+/* conditions are 
+if time is between 6 and 12 , then print Good morning! 
+if time is between 12 and 6 , them print Good afternoon!
+else good evening
+*/
+let hour = 3;
+
+if (hour >= 6 && hour < 12) console.log("Good Morning");
+else if (hour >= 12 && hour < 6) console.log("Good Afternoon");
+else console.log("Good Evening");
