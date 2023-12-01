66ef0edfc83267c87dd52ebc7ca0616c17bf04d5
Asim Nazeer <asimnazeer925@gmail.com>
2023-12-01 23:19:42 +0500

    Factory & Constructor FUNCTIONS

--- Object/index.js
+++ Object/index.js
@@ -1,14 +1,12 @@
-// factory Functions
+// FACTORY FUNCTION
 
 /* 
-
 function noramaly aisa hota hai
  function name(parameter) {} 
 
 agr object ke andar function likhna ho to direct
 name() {}  || name = function()
 or ye method kehlaye ga
-
 */
 
 /* (line: 12) agr yahan me return ke bajaye object ka naam rakh do to return object_name; krna pare ga is liye direct ye karo 
@@ -30,3 +28,22 @@ console.log(circle1);
 
 const circle2 = createCircle(2);
 console.log(circle2);
+
+// ========================================================================================
+// CONSTRUCTOR FUNCTION
+
+/* Pascal notation (OneTwoThree....) use hoti hai constructor function main
+'new' lagane se aik empty object bane ga jo ke 'Circle(radius)' ko call akre ga apne adar
+phir ye 'this' jo hai khud hi property banade ga jaise "radius = radius;"
+or phir return kare ga sari "this" wali properties ko 
+AGR in ko dekhna ho to name.constructor karo console pr
+*/
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const circle = new Circle(1);
