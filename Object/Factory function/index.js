fba384fb74a31c733de359168aa92541d62218e8
Asim Nazeer <asimnazeer925@gmail.com>
2023-12-01 22:37:12 +0500

    Factory Function

---
+++ Object/Factory function/index.js
@@ -0,0 +1,26 @@
+// factory Functions
+
+/* function noramaly aisa hota hai
+function name(parameter) {} 
+
+agr object ke andar function likhna ho to direct
+name() {} 
+or ye method kehlaye ga
+// */
+
+// function createCircle(radius) {
+//  /* agr yahan me return ke bajaye object ka naam rakh do to return object_name; krna pare ga is liye direct ye karo */
+//   return {
+//     radius, /* yahan mere pas parameter or us ki value dono same hain yani "radius: radius;" isliye direct parameter */
+//     draw() {
+//       /* ye method banaya hai maine (details on top line: 3) */
+//       console.log("draw");
+//     },
+//   };
+// }
+
+// const circle1 = createCircle(1);
+// console.log(circle1);
+
+// const circle2 = createCircle(2);
+// console.log(circle2);
