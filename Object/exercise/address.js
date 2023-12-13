0f49366ea124399674290ab16f5b4944eb5553bf
Asim Nazeer <asimnazeer925@gmail.com>
2023-12-13 23:11:28 +0500

    exercises about factory and contructor function

--- Object/exercise/address.js
+++ Object/exercise/address.js
@@ -1,17 +1,43 @@
 // ===========================================================================
-// EXERCISE 1
+// EXERCISE 1 && 2
 
 // street , city , zip code
 // showAddress(address)
 
-let address = {
-  street: "Mehmoodabad no.6",
-  city: "Karachi",
-  zipCode: "90001",
-};
+// let address = {
+//   street: "Mehmoodabad no.6",
+//   city: "Karachi",
+//   zipCode: "90001",
+// };
 
-function showAddress(address) {
-  for (let key in address) console.log(key, ":", address[key]);
+// function showAddress(address) {
+//   for (let key in address) console.log(key, ":", address[key]);
+// }
+
+// showAddress(address);
+
+// ===========================================================================
+// by factory function
+
+// function showAddress(street, city, zipcode) {
+//   return {
+//     street,
+//     city,
+//     zipcode,
+//   };
+// }
+
+// const addressTemp = showAddress("mehmoodabad 6", "karachi", "90001");
+// console.log(addressTemp);
+
+// ===========================================================================
+// by constructor function
+
+function ShowAddress(street, city, zipcode) {
+  this.street = street;
+  this.city = city;
+  this.zipcode = zipcode;
 }
 
-showAddress(address);
+const address = new ShowAddress("mehmoodabad 6", "karachi", "90001");
+console.log(address);
