cb989258fde99dd2b1dcc3d26b363e6b8040e334
Asim Nazeer <asimnazeer925@gmail.com>
2023-12-14 23:20:56 +0500

    3 exercises done

--- Object/exercise/address.js
+++ Object/exercise/address.js
@@ -33,11 +33,38 @@
 // ===========================================================================
 // by constructor function
 
-function ShowAddress(street, city, zipcode) {
+// function ShowAddress(street, city, zipcode) {
+//   this.street = street;
+//   this.city = city;
+//   this.zipcode = zipcode;
+// }
+
+// const address = new ShowAddress("mehmoodabad 6", "karachi", "90001");
+// console.log(address);
+
+// ===========================================================================
+// ANOTHER Exercise
+
+function Address(street, city, zipcode) {
   this.street = street;
   this.city = city;
   this.zipcode = zipcode;
 }
 
-const address = new ShowAddress("mehmoodabad 6", "karachi", "90001");
-console.log(address);
+let address1 = new Address("a", "b", "c");
+let address2 = new Address("a", "b", "c");
+
+console.log(areEqual(address1, address2));
+console.log(areSame(address1, address2));
+
+function areEqual(address1, address2) {
+  return (
+    address1.street === address2.street && // agr to ye sab true hoga to hi log hoga
+    address1.city === address2.city && // baghair if ke is tarhan kr liya GEO
+    address1.zipcode === address2.zipcode
+  );
+}
+
+function areSame(address1, address2) {
+  return address1 === address2; // ye dono alag objects hai is waja se false aye ga
+}
