3766ba3d97d9c1af40b9b4eb90c41f1092a44fff
Asim Nazeer <asimnazeer925@gmail.com>
2023-12-21 17:42:48 +0500

    playing with arrays

--- Arrays/index.js
+++ Arrays/index.js
@@ -52,7 +52,7 @@
 //
 // console.log(detail);
 
-// == Easy way == (finding primitive elements)
+// == Easy way == (finding primitive elements) => (by arrow)
 
 // const detail = details.find((details) => details.name === "a");
 // console.log(detail);
@@ -87,3 +87,92 @@
 // let anywhere = group.splice(2, 2); // 2nd index se start ho kr 2 or delete honge
 // console.log(group);
 // console.log(anywhere);
+
+// ========================================================================================
+//REMOVING ALL ELEMENTS OF THE ARRAY
+
+// let numbers = [1, 2, 3, 4];
+// let another = numbers;
+
+// solution 1
+// numbers= []; // !important
+// console.log(number);
+
+// solution 2
+// numbers.length = 0; // !important
+// console.log(numbers);
+
+// solution 3
+// numbers.splice(0, numbers.length);
+// console.log(numbers);
+
+// solution 4
+// while (numbers.length > 0) numbers.pop();
+// console.log(numbers);
+
+// ========================================================================================
+// slicing or combining the array with HARD MEHTOD: PRIMITIVE TYPE
+
+// const first = [1, 2, 3];
+// const last = [4, 5, 6];
+
+// let combined = first.concat(last); // combine first with last
+// let sliced = combined.slice(3);
+// // combined.slice(); se poora array wapis
+// // combined.slice(3, 5); se 3rd element se 5th tak print hoga
+
+// console.log("first array : " + first);
+// console.log("last array : " + last);
+// console.log("====>");
+// console.log("combined array : " + combined);
+// console.log("====>");
+// console.log("slicing the combined one : " + sliced);
+
+// ========================================================================================
+// slicing and combining the array using easy (spread) method : PRIMITIVE TYPE
+
+// let first = [1, 2];
+// let last = [3, 4];
+
+// // let combined = first.concat(last);
+// let combined = [...first, ...last];
+// console.log(combined);
+
+// // combined.slice();
+// let copy = [...combined];
+// console.log(copy);
+
+// ========================================================================================
+// looping array
+
+// const numbers = ["a", "b", "c", "d"];
+
+// // == WAY 1 ==>
+// for (let number of numbers) {
+//   console.log("Element : " + number);
+//   console.log("Index : " + numbers.indexOf(number));
+//   console.log("===");
+// }
+
+// // == WAY 2 ==>
+// numbers.forEach((number, index) =>
+//   console.log(
+//     "element : " + number,
+//     "||===>" + " " + "index of the element is : " + index
+//   )
+// );
+
+// ========================================================================================
+// Joining elements of the Array
+
+// let array = [1, 2, 3, 4];
+//
+// let joined = array.join(" , ");
+// console.log(joined);
+
+let message = "papa dabbu hain";
+let parts = message.split(" ");
+console.log(parts);
+
+let combined = parts.join("-");
+console.log(combined);
