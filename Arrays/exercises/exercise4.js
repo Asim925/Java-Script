8939f73d98c1da724206f8b42f3a69dddfaebb3f
Asim Nazeer <asimnazeer925@gmail.com>
2024-01-04 01:15:51 +0500

    recreated exercise 4

--- Arrays/exercises/exercise4.js
+++ Arrays/exercises/exercise4.js
@@ -1,18 +1,28 @@
+// function move(array, index, offset) {
+//   let position = index + offset;
+//   if (position >= numbers.legnth || position < 0)
+//     console.error("invalid offset");
+
+//   const output = [...array];
+//   let deleted = output.splice(index, 1)[0]; // returned array ki Index:0 value ; [0]
+//   output.splice(position, 0, deleted);
+//   return output;
+// }
+
 const numbers = [1, 2, 3, 4];
 
-const output = move(numbers, 0, 4);
+const output = move(numbers, 1, -4);
 
 console.log(output);
 
+// function
+
 function move(array, index, offset) {
   let position = index + offset;
-  if (position >= numbers.legnth || position < 0)
-    console.error("invalid offset");
+  if (position > array.length || position < 0) console.error("invalid offset");
 
   const output = [...array];
-  let deleted = output.splice(index, 1)[0]; // returned array ki Index:0 value ; [0]
+  deleted = output.splice(index, 1)[0];
   output.splice(position, 0, deleted);
   return output;
 }
-
-// IS SE ZIADA MUSHKIL NAHI DEKHA KAL PHIR KAROON GA KHUD
