7a259b931323c6e45c01e520275f658699d1dad6
Asim Nazeer <asimnazeer925@gmail.com>
2024-01-05 00:13:30 +0500

    last exercise nt successful

---
+++ Arrays/exercises/exercise7.js
@@ -0,0 +1,16 @@
+const movies = [
+  { title: "a", year: 2018, rating: 4.5 },
+  { title: "b", year: 2018, rating: 4.7 },
+  { title: "c", year: 2018, rating: 3 },
+  { title: "d", year: 2017, rating: 4.5 },
+];
+let movie1;
+if(movies[0].year&&movies[0].rating)
+
+
+/* 
+bhut dimagh ghuum gaya ab soch soch ke sar 
+main dard horaha hai subah se logic pr logic ,
+logic pr logic , logic pr logic , 
+bas KULLU KHALLAS HAJJEI 
+*/
