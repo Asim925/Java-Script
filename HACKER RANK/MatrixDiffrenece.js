b3d209d9a10a5a614de2d515a666c783296d160e
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-01 12:14:48 +0500

    matrix difference of diagnols

--- HACKER RANK/MatrixDiffrenece.js
+++ HACKER RANK/MatrixDiffrenece.js
@@ -1,3 +1,17 @@
-/* pehle is keliye stars wali exercise ko samajhna hoga wapis ja kr mere khayal se then i wil do something
-and i am just loving that typing things and i am  still typing with thirty wpm 
-hence proved that i am dabbu bye bye */
+function diagonalDifference(arr) {
+  let leftToRightDiagnol = arr[0][0] + arr[1][1] + arr[2][2];
+  let rightToLeftDiagnol = arr[0][2] + arr[1][1] + arr[2][0];
+  let difference = leftToRightDiagnol - rightToLeftDiagnol;
+  if (difference < 0) difference = rightToLeftDiagnol - leftToRightDiagnol;
+
+  return difference;
+}
+
+let array = [
+  [11, 2, 4, 5],
+  [4, 5, 6, 6],
+  [10, 8, 12, 6],
+  [10, 8, 12, 6],
+];
+
+console.log(diagonalDifference(array));
