c457cca3f82baa3d6db0167001663fcbe8b2b372
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-06 21:50:27 +0500

    Time Conversion (perfection)

--- HACKER RANK/Time conversion.js
+++ HACKER RANK/Time conversion.js
@@ -1,27 +1,31 @@
 function timeConversion(s) {
   let splitted = s.split(":");
+  let hour = parseInt(splitted[0], 10);
 
-  if (splitted[0] === "12") {
-    let replaced = s.replace("12", "00");
-    return replaced;
-  }
-  if (s.includes("AM") || raplaced.includes("AM")) {
-    return s.replace("AM", "") || replaced.replace("AM", "");
+  if (s.includes("PM") && hour === 12) {
+    return s.replace("PM", "");
   }
 
-  if (s.includes("PM") && s[0] < 12) {
+  if (s.includes("AM")) {
+    if (hour <= "12") {
+      let replaced = s.replace("12", "00");
+      return replaced.replace("AM", "");
+    }
+  }
+
+  if (s.includes("PM") && !hour < 12) {
     let newtime = s.replace("PM", ""); // PM ==> ""
     let number = newtime.split(":")[0]; // targetting the first element to add 12 in
-    let numberINT = parseInt(number);
+    let numberINT = parseInt(number); // converting string to number
 
-    let changednumber = numberINT + 12;
-    let STRnumber = changednumber.toString();
-    if (STRnumber === "24") STRnumber = "00";
-    let miltitaryTime = newtime.replace(number, STRnumber);
-    return miltitaryTime;
+    let changednumber = numberINT + 12; // adding 12
+    let STRnumber = changednumber.toString(); // converting number to stringn
+    if (STRnumber === "24") STRnumber = "00"; // if 24 then === 00
+    let miltitaryTime = newtime.replace(number, STRnumber); //replacig the hour to the militry format
+    return miltitaryTime; // kullu khallas
   }
 }
 
-let s = "12:00:00AM";
+let s = "07:00:00PM";
 
 console.log(timeConversion(s));
