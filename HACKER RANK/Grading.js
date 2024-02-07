4bea3658bc669b751b01c843d21a6e041f13193a
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-07 17:46:38 +0500

    wait untill my mind set succescfully <3

---
+++ HACKER RANK/Grading.js
@@ -0,0 +1,9 @@
+function gradingStudents(grades) {
+  for (let i = 0; i <= grades.length; i++) console.log(grades[i] % 5);
+  // if (grades[i] >= 38) {
+  // if (grades[i] % 5 >= 2) {
+  // }
+}
+
+let grades = [73, 67, 38, 33];
+gradingStudents(grades);
