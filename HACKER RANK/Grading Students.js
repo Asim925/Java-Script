a7e7e7ca0224e2e0a2cef40d774df1993aea2ee1
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-09 01:13:23 +0500

    Frequency (easy mode)

---
+++ HACKER RANK/Grading Students.js
@@ -0,0 +1,8 @@
+function gradingStudents(grades) {
+  for (let i = 0; i < grades.length; i++) {
+    console.log(grades[i]);
+  }
+}
+
+let grades = [53, 53, 35];
+gradingStudents(grades);
--- HACKER RANK/Grading.js
+++
@@ -1,9 +0,0 @@
-function gradingStudents(grades) {
-  for (let i = 0; i <= grades.length; i++) console.log(grades[i] % 5);
-  // if (grades[i] >= 38) {
-  // if (grades[i] % 5 >= 2) {
-  // }
-}
-
-let grades = [73, 67, 38, 33];
-gradingStudents(grades);
