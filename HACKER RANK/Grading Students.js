9a2d8af421cd8118354ba1f25b09939808529e38
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-10 17:12:51 +0500

    Grading students

--- HACKER RANK/Grading Students.js
+++ HACKER RANK/Grading Students.js
@@ -1,8 +1,14 @@
 function gradingStudents(grades) {
-  for (let i = 0; i < grades.length; i++) {
-    console.log(grades[i]);
+  for (let grade of grades) {
+    if (!(grade < 38) && grade % 5 >= 3) {
+      for (let i = 3; i <= 5; i++) {
+        for (let j = 0; j <= 2; j++) if (grade % 5 === i) grade += j;
+      }
+      if (!(grade < 38) && grade % 5 <= 3) console.log(grade);
+      // console.log(grade);
+    }
   }
 }
 
-let grades = [53, 53, 35];
+let grades = [37, 38, 39];
 gradingStudents(grades);
