1c8e6e3d01deb0f9cd3c0d9d8a0826665ff5ad9e
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-14 01:21:20 +0500

    wait

---
+++ HACKER RANK/Designer PDF Viewer.js
@@ -0,0 +1,40 @@
+function designerPdfViewer(h, word) {
+  if (word.length <= 10) {
+    let splitted = word.split("");
+    for (let i = 0; i < h.length; i++) {
+      let a = h[i];
+      let b = h[i];
+      let c = h[i];
+      let d = h[i];
+      let e = h[i];
+      let f = h[i];
+      let g = h[i];
+      // let h = [i];
+      // let i = [i];
+      let j = h[i];
+      let k = h[i];
+      let l = h[i];
+      let m = h[i];
+      let n = h[i];
+      let o = h[i];
+      let p = h[i];
+      let q = h[i];
+      let r = h[i];
+      let s = h[i];
+      let t = h[i];
+      let u = h[i];
+      let v = h[i];
+      let w = h[i];
+      let x = h[i];
+      let y = h[i];
+      let z = h[i];
+    }
+  }
+}
+
+let h = [
+  1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7,
+];
+let word = "zaba";
+
+console.log(designerPdfViewer(h, word));
