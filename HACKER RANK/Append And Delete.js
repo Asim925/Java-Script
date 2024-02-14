802ff084962ad921b7e93df3fd4a27315ab57beb
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-14 23:25:39 +0500

    wait

---
+++ HACKER RANK/Append And Delete.js
@@ -0,0 +1,11 @@
+function appendAndDelete(s, t, k) {
+  let sArray = s.split("");
+  let tArray = t.split("");
+
+  let y = sArray.splice();
+  console.log(y);
+}
+
+let s = "hackerhappy";
+let t = "hackerrank";
+console.log(appendAndDelete(s, t, 9));
