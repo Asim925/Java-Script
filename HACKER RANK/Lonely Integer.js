8dd40195b9b9266c648c541446b7797645d6c89f
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-17 00:41:19 +0500

    Lonely Integer

--- HACKER RANK/Lonely Integer.js
+++ HACKER RANK/Lonely Integer.js
@@ -1,17 +1,9 @@
 function lonelyinteger(a) {
-  a.sort((a, b) => a - b);
   let unique = 0;
-  if (a.length <= 1) unique = a[0];
-  else
-    for (let i = 0; i < a.length; i++)
-      for (let j = i + 1; j < a.length; j++) {
-        if (a[i] !== a[j]) unique = a[j];
-      }
-
+  for (let i = 0; i < a.length; i++)
+    if (a.indexOf(a[i]) === a.lastIndexOf(a[i])) unique = a[i];
   return unique;
 }
 
-let a = [
-  34, 95, 34, 64, 45, 95, 16, 80, 80, 75, 3, 25, 75, 25, 31, 3, 64, 16, 31,
-];
+let a = [1, 12, 1, 2, 12];
 console.log(lonelyinteger(a));
