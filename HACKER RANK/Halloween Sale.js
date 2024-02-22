158649a12e60a73086acdc8fbaa7741232e157f9
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-22 13:32:14 +0500

    Halloween Sale(incomplete)

--- HACKER RANK/Halloween Sale.js
+++ HACKER RANK/Halloween Sale.js
@@ -1,8 +1,30 @@
 function howManyGames(p, d, m, s) {
-  let startPrice = p;
+  let startingPrice = p;
   let discount = d;
   let minimum = m;
   let budget = s;
+
+  let prices = [];
+  for (let i = startingPrice; i >= 0; i -= discount) {
+    if (i >= minimum) prices.push(i);
+    if (i < minimum) {
+      let dif = minimum - i;
+      let added = dif + i;
+      prices.push(added);
+    }
+  }
+  console.log(prices);
+
+  let newArr = [];
+  for (let i = 0; i < prices.length; i++) {
+    newArr.push(prices[i]);
+    let reduced = newArr.reduce((a, b) => a + b);
+    if (reduced > budget) {
+      newArr.pop();
+      console.log(newArr);
+    }
+  }
+  return newArr.length;
 }
 
-console.log(howManyGames(20, 3, 6, 85));
+console.log(howManyGames(89, 60, 27, 7777));
