74e28b1558a5e9439352ef482a6e9db59b129939
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-26 22:31:41 +0500

    10 Days of JS

--- HACKER RANK/EXTRAS/10 Days Of JS.js
+++ HACKER RANK/EXTRAS/10 Days Of JS.js
@@ -1,15 +1,27 @@
-function vowelsAndConsonants(s) {
-  let splitted = s.split("");
-  let vowels = ["a", "e", "i", "o", "u"];
-  for (let i = 0; i < splitted.length; i++)
-    for (let n of vowels)
-      if (splitted[i] === n) {
-        let index = splitted.indexOf(splitted[i]);
-        let element = splitted.splice(index, 1)[0];
-        splitted.unshift(element);
-      }
-  console.log(splitted);
+// =============== GRADES ==================//
+
+// function getGrade(score) {
+//   let grade;
+//   if (score > 25 && score <= 30) grade = "A";
+//   if (score > 20 && score <= 25) grade = "B";
+//   if (score > 15 && score <= 20) grade = "C";
+//   if (score > 10 && score <= 15) grade = "D";
+//   if (score > 5 && score <= 10) grade = "E";
+//   if (score >= 0 && score <= 5) grade = "F";
+
+//   return grade;
+// }
+
+// console.log(getGrade(28));
+
+// =============== secondLargest ==================//
+function getSecondLargest(nums) {
+  let single = nums
+    .filter((items, index) => nums.indexOf(items) === index)
+    .sort((a, b) => b - a);
+  single.shift();
+  return single[0];
 }
 
-let s = "javascriptLoops";
-vowelsAndConsonants(s);
+let nums = [2, 3, 6, 6, 5];
+console.log(getSecondLargest(nums));
