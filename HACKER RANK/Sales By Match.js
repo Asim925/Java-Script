939887af247ba343a7acdf6b6d4b3dae761b2131
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-28 15:34:49 +0500

    Sales By Match

--- HACKER RANK/Sales By Match.js
+++ HACKER RANK/Sales By Match.js
@@ -1,10 +1,18 @@
 function sockMerchant(n, ar) {
-  ar.sort();
-  for (let i = 0; i < n; i++) {
-    let filter = ar.filter((n) => n > ar[i]);
-    console.log(filter);
+  let count = 0;
+  ar.sort((a, b) => a - b);
+  for (let i = 0; i < n; i += 2) {
+    if (ar[i] === ar[i + 1]) console.log([ar[i], ar[i + 1]]);
   }
+  return count;
 }
 
-let ar = [1, 1, 1, 2, 2, 3];
-console.log(sockMerchant(6, ar));
+let ar = [
+  44, 55, 11, 15, 4, 72, 26, 91, 80, 14, 43, 78, 70, 75, 36, 83, 78, 91, 17, 17,
+  54, 65, 60, 21, 58, 98, 87, 45, 75, 97, 81, 18, 51, 43, 84, 54, 66, 10, 44,
+  45, 23, 38, 22, 44, 65, 9, 78, 42, 100, 94, 58, 5, 11, 69, 26, 20, 19, 64, 64,
+  93, 60, 96, 10, 10, 39, 94, 15, 4, 3, 10, 1, 77, 48, 74, 20, 12, 83, 97, 5,
+  82, 43, 15, 86, 5, 35, 63, 24, 53, 27, 87, 45, 38, 34, 7, 48, 24, 100, 14, 80,
+  54,
+];
+console.log(sockMerchant(100, ar));
