622865b8516c0368452d279feaa8c345a17e4f76
Asim Nazeer <asimnazeer925@gmail.com>
2024-02-29 22:57:44 +0500

    Save The Prisnor!

--- HACKER RANK/Save The Prisnor!.js
+++ HACKER RANK/Save The Prisnor!.js
@@ -1,8 +1,12 @@
 function saveThePrisoner(n, m, s) {
+  // s : starting seat
+  // n : number of prisnors
+  // m : candies
   let currentPrisoner = (s + m - 1) % n;
   if (currentPrisoner === 0) {
     currentPrisoner = n;
   }
   return currentPrisoner;
 }
-console.log(saveThePrisoner(4, 6, 2));
+console.log(saveThePrisoner(5, 3, 4));
+//  shru 4 se hongi mtlb pehli candy 4th prisnor ki
