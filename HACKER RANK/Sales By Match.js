6aa116bc8ff94686dad3d21708e1ed095bf62688
Asim Nazeer <asimnazeer925@gmail.com>
2024-03-01 20:51:32 +0500

    Sales By Match

--- HACKER RANK/Sales By Match.js
+++ HACKER RANK/Sales By Match.js
@@ -1,18 +1,34 @@
+// ================== MY CODE ================== //
 function sockMerchant(n, ar) {
   let count = 0;
-  ar.sort((a, b) => a - b);
-  for (let i = 0; i < n; i += 2) {
-    if (ar[i] === ar[i + 1]) console.log([ar[i], ar[i + 1]]);
+  let singles = ar.filter((value, index) => ar.indexOf(value) === index);
+  for (let single of singles) {
+    let singleElements = ar.filter((n) => n === single);
+    let pairsCount = Math.floor(singleElements.length / 2);
+    count += pairsCount;
   }
   return count;
 }
 
-let ar = [
-  44, 55, 11, 15, 4, 72, 26, 91, 80, 14, 43, 78, 70, 75, 36, 83, 78, 91, 17, 17,
-  54, 65, 60, 21, 58, 98, 87, 45, 75, 97, 81, 18, 51, 43, 84, 54, 66, 10, 44,
-  45, 23, 38, 22, 44, 65, 9, 78, 42, 100, 94, 58, 5, 11, 69, 26, 20, 19, 64, 64,
-  93, 60, 96, 10, 10, 39, 94, 15, 4, 3, 10, 1, 77, 48, 74, 20, 12, 83, 97, 5,
-  82, 43, 15, 86, 5, 35, 63, 24, 53, 27, 87, 45, 38, 34, 7, 48, 24, 100, 14, 80,
-  54,
-];
-console.log(sockMerchant(100, ar));
+let ar = [1, 2, 1, 2, 1, 3, 2];
+console.log(sockMerchant(7, ar));
+
+// ================== sheikhs CODE ================== //
+
+// function sockMerchant(n, ar) {
+//   let sockCounts = {};
+//   let pairsCount = 0;
+
+//   for (let sock of ar) {
+//     sockCounts[sock] = (sockCounts[sock] || 0) + 1;
+//   }
+
+//   for (let count of Object.values(sockCounts)) {
+//     pairsCount += Math.floor(count / 2);
+//   }
+
+//   return pairsCount;
+// }
+
+// let ar = [1, 2, 1, 2, 1, 3, 2];
+// console.log(sockMerchant(7, ar));
