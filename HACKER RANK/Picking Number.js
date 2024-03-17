f82f2632d8da6c32792560eea720ec98b6d05bb7
Asim Nazeer <asimnazeer925@gmail.com>
2024-03-18 03:15:04 +0500

    Picking The Numbers

---
+++ HACKER RANK/Picking Number.js
@@ -0,0 +1,13 @@
+// function pickingNumbers(a) {
+//   a.sort((a, b) => a - b);
+//   for (let i = 0; i < a.length; i++) {
+//     let current = a[i];
+//     if (a[i + 1] === current + 1 || a[i + 1] === current) continue;
+//     else {
+//       let part = a.splice(0, a[i + 1]);
+//       console.log(part);
+//     }
+//   }
+// }
+
+// let a = [1, 1, 2, 2, 4, 4, 5, 5, 5];
