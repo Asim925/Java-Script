e0d8c1a08d26cb5b77d205c8ffc14170abfb6a58
Asim Nazeer <asimnazeer925@gmail.com>
2024-04-26 16:57:54 +0500

    XOR-Sequence

--- HACKER RANK/XOR-Sequence.js
+++ HACKER RANK/XOR-Sequence.js
@@ -1,1 +1,15 @@
-A = [0, 1, 3, 0, 4, 1, 7, 0, 8, 1, 11, 0, 12, 11];
+function xorSequence(l, r) {
+  let sum = 0;
+  let A = [];
+  for (let i = 0; i <= r; i++) {
+    if (i === 0) {
+      A.push(0);
+      continue;
+    }
+    A.push(A[i - 1] ^ i);
+    if (i >= l) sum = sum ^ A[i];
+  }
+  return sum;
+}
+
+console.log(xorSequence(39827027514126, 562796524793506));
