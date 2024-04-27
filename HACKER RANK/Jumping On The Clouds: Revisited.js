9b24ca454bdada8ee58ba4d3bc3a3e2eff2bcaaa
Asim Nazeer <asimnazeer925@gmail.com>
2024-04-27 21:21:41 +0500

    Jumping On The Clouds: Revisited

--- HACKER RANK/Jumping On The Clouds: Revisited.js
+++ HACKER RANK/Jumping On The Clouds: Revisited.js
@@ -1,11 +1,30 @@
+//  SASTA hacker
+
+// function jumpingOnClouds(c, k) {
+//   let n = c.length;
+//   let e = 100;
+//   for (let i = 0; i < n; i += k) {
+//     let cloud = c[(i + k) % n];
+//     if (c[n - 1] === cloud) cloud = c[0];
+
+//     if (cloud === 1) e -= 2;
+//     e--;
+//   }
+//   return e;
+// }
+
+// ======= SHIEKH SAHAB++=
+
 function jumpingOnClouds(c, k) {
   let n = c.length;
   let e = 100;
-  for (let i = 0; i < n; i += k) {
-    let cloud = c[(i + k) % n];
+  let i = 0;
+  do {
+    let cloud = c[i];
     if (cloud === 1) e -= 2;
     e--;
-  }
+    i = (i + k) % n;
+  } while (i !== 0);
   return e;
 }
 
