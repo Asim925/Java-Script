9a520685dae18e1b55b2782185f9b71e690f5ba2
Asim Nazeer <asimnazeer925@gmail.com>
2024-05-14 23:15:21 +0500

    control flow

--- control flow/index.js
+++ control flow/index.js
@@ -309,30 +309,32 @@ property print karwani ho to for in use karo*/
 // ========================= //
 // EXERCISE LAST (11);-
 
-// showPrimes(20);
-
-// function showPrimes(limit) {
-//   for (let number = 2; number <= limit; number++)
-//     if (isPrime(number)) console.log(number);
-// }
-
-// function isPrime(number) {
-//   for (let factor = 2; factor < number; factor++)
-//     if (number % factor === 0) return false;
-//   return true;
-// }
-
 showPrimes(20);
 
 function showPrimes(limit) {
-  for (let number = 2; number <= limit; number++) {
-    let isPrime = true;
-    for (let factor = 2; factor < number; factor++) {
-      if (number % factor === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    if (isPrime) console.log(number);
-  }
+  for (let number = 2; number <= limit; number++)
+    if (isPrime(number) === true) console.log(number);
 }
+
+function isPrime(number) {
+  let isPrime = true;
+  for (let factor = 2; factor < number; factor++) {
+    if (number % factor === 0) isPrime = false;
+  }
+  return isPrime;
+}
+
+// showPrimes(20);
+
+// function showPrimes(limit) {
+//   for (let number = 2; number <= limit; number++) {
+//     let isPrime = true;
+//     for (let factor = 2; factor < number; factor++) {
+//       if (number % factor === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if (isPrime) console.log(number);
+//   }
+// }
