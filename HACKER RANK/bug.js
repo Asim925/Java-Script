dfb246b01ec6c61f3b8d5eb0ed499f1136d84446
Asim Nazeer <asimnazeer925@gmail.com>
2024-05-22 23:38:49 +0500

    bug.js

---
+++ HACKER RANK/bug.js
@@ -0,0 +1,12 @@
+function last(input) {
+  input = input.trim();
+  let length = 0;
+  for (let i = input.length - 1; i >= 0; i--) {
+    // should be --
+    if (input[i] !== " ") length++;
+    else if (+length > 0) break;
+  }
+  console.log(length);
+}
+
+last("Shine on ypu crazy diamond");
