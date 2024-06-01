b79962157d68287b7fe2b94cd3933011132ae5e5
Asim Nazeer <asimnazeer925@gmail.com>
2024-06-02 00:57:26 +0500

    Sherlock and array

--- HACKER RANK/Sherlock And Array.js
+++ HACKER RANK/Sherlock And Array.js
@@ -1,16 +1,15 @@
 function balancedSums(arr) {
-  let n = arr.length;
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < arr.length; i++) {
     let part1 = arr.slice(0, i);
-    if (part1.length === 0) part1 = [0];
-    let part2 = arr.slice(i + 1, n);
-    if (part2.length === 0) part2 = [0];
-
-    if (part1.reduce((a, b) => a + b) === part2.reduce((a, b) => a + b))
-      return "YES";
+    let part2 = arr.slice(i + 1, arr.length);
+    let sumOfPart1 = 0;
+    if (part1.length) sumOfPart1 = part1.reduce((a, b) => a + b);
+    let sumOfPart2 = 0;
+    if (part2.length) sumOfPart2 = part2.reduce((a, b) => a + b);
+    if (sumOfPart1 === sumOfPart2) return "YES";
   }
   return "NO";
 }
 
-let arr = [4, 4, 4, 4];
+let arr = [1, 1, 4, 1, 1];
 console.log(balancedSums(arr));
