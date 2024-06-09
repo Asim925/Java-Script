d77aad1fdee8a054794b543cd4e7da1d0b2d7dff
Asim Nazeer <asimnazeer925@gmail.com>
2024-06-09 20:28:46 +0500

    Hacker rank in a string

--- HACKER RANK/HackerRank in a String!.js
+++ HACKER RANK/HackerRank in a String!.js
@@ -1,14 +1,18 @@
 function hackerrankInString(s) {
+  s = s.split("");
+  let obj = {};
   let hackerrank = ["h", "a", "c", "k", "e", "r", "r", "a", "n", "k"];
-  if (s.match(/r/g).length < 2) return "NO";
+  for (let letter of s)
+    if (hackerrank.includes(letter)) obj[letter] = (obj[letter] || 0) + 1;
 
-  for (let i = 0; i < hackerrank.length; i++) {
-    let currentIndex = s.indexOf(hackerrank[i]);
-    let nextIndex = s.lastIndexOf(hackerrank[i + 1]);
-    if (!s.includes(hackerrank[i]) || currentIndex > nextIndex) {
-      if (nextIndex < 0) continue;
-      return "NO";
-    }
+  for (let letter in obj) {
+    if (letter === "r" && !(obj[letter] >= 2)) return "NO";
+    else if (letter === "h" && !(obj[letter] >= 1)) return "NO";
+    else if (letter === "a" && !(obj[letter] >= 2)) return "NO";
+    else if (letter === "n" && !(obj[letter] >= 1)) return "NO";
+    else if (letter === "k" && !(obj[letter] >= 1)) return "NO";
+    else if (letter === "e" && !(obj[letter] >= 1)) return "NO";
+    else if (letter === "c" && !(obj[letter] >= 1)) return "NO";
   }
   return "YES";
 }
