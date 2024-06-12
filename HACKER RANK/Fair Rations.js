3a4c530fb019df8350ce772fa88166db43fad90c
Asim Nazeer <asimnazeer925@gmail.com>
2024-06-12 16:24:53 +0500

    Time In Words

---
+++ HACKER RANK/Fair Rations.js
@@ -0,0 +1,14 @@
+// function fairRations(B) {
+//   let count = 0;
+//   for (let i = 0; i < B.length - 1; i++) {
+//     if (B[i] % 2 === 0) {
+//       count++;
+//       B[i] = B[i] + 1;
+//       B[i + 1] = B[i + 1] + 1;
+//     }
+//   }
+// }
+
+// console.log(fairRations([2, 3, 4, 5, 6]));
+
+console.log(10 % 1);
