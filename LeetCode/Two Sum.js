e38a4540258cc840fdd4989fa241c4b123ff32d6
Asim Nazeer <asimnazeer925@gmail.com>
2024-09-06 04:12:55 +0500

    Two Sum

---
+++ LeetCode/Two Sum.js
@@ -0,0 +1,7 @@
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++)
+    for (let j = 0; j < nums.length; j++)
+      if (i !== j && nums[i] + nums[j] === target) return [i, j];
+};
+
+console.log(twoSum([3, 3], 6));
