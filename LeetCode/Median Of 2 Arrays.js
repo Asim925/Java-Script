07d14f4a909f6c297efb5d2dac33fb1cbfc4952e
Asim Nazeer <asimnazeer925@gmail.com>
2024-09-06 04:49:16 +0500

    Roman To INT (unsolved)

---
+++ LeetCode/Median Of 2 Arrays.js
@@ -0,0 +1,10 @@
+var findMedianSortedArrays = function (nums1, nums2) {
+  let array = [...nums1, ...nums2].sort((a, b) => a - b);
+  let center = Math.floor(array.length / 2);
+
+  if (array.length % 2 !== 0) return array[center].toFixed(5);
+
+  return ((array[center] + array[center - 1]) / 2).toFixed(5);
+};
+
+console.log(findMedianSortedArrays([1, 2], [3, 4]));
